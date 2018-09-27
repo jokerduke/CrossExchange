3780c333eb4b3073c93b272e72cfea14dbef4ca5
Anshul Jain <anshjain@expedia.com>
2018-09-27 06:03:47 +0530

    Initial Commit

---
+++ XOProject/Controller/TradeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace XOProject.Controller
+{
+    [Route("api/Trade/")]
+    public class TradeController : ControllerBase
+    {
+        private IShareRepository _shareRepository { get; set; }
+        private ITradeRepository _tradeRepository { get; set; }
+        private IPortfolioRepository _portfolioRepository { get; set; }
+
+        public TradeController(IShareRepository shareRepository, ITradeRepository tradeRepository, IPortfolioRepository portfolioRepository)
+        {
+            _shareRepository = shareRepository;
+            _tradeRepository = tradeRepository;
+            _portfolioRepository = portfolioRepository;
+        }
+
+
+        [HttpGet("{portfolioid}")]
+        public async Task<IActionResult> GetAllTradings([FromRoute]int portFolioid)
+        {
+            var trade = _tradeRepository.Query().Where(x => x.PortfolioId.Equals(portFolioid));
+            return Ok(trade);
+        }
+
+
+        /// <summary>
+        /// For a given symbol of share, get the statistics for that particular share calculating the maximum, minimum, 
+        /// average and Sum of all the trades for that share individually grouped into Buy trade and Sell trade.
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+
+        [HttpGet("Analysis/{symbol}")]
+        public async Task<IActionResult> GetAnalysis([FromRoute]string symbol)
+        {
+            var result = new List<TradeAnalysis>();
+            
+            return Ok(result);
+        }
+
+
+    }
+}
